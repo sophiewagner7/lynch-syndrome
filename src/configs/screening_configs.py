3e73f69dbfe4e32642f995c6008caf74b74f40b0
sophiewagner7 <sophie.wagn@gmail.com>
2025-08-18 08:38:22 -0400

    feat: read in screening strategies and test specs from JSON and create strategy class to hold specs

--- src/configs/screening_configs.py
+++ src/configs/screening_configs.py
@@ -1,127 +1,146 @@
 import logging
+import json
 
-# TODO: Make this read in from csv/xlsx
-# TODO: Make test performance optional input in main.py
-# Screening test specifications
-SCREENING_TEST_SPECS = {
-    "FIT": {
-        "sens_LRpolyp": 0.1,
-        "sens_HRpolyp": 0.2,
-        "sens_stage_1": 0.79,
-        "sens_stage_2": 0.79,
-        "sens_stage_3": 0.79,
-        "sens_stage_4": 0.79,
-        "spec": 0.95,
-        "fpr": 0.05,
-        "c_test": 20,
-        "du_test": 0,
-        "p_complication": 0,
-        "du_complication": 0,
-        "p_death_complication": 0,
-        "c_complication": 0,
-    },
-    "Colo": {
-        "sens_LRpolyp": 0.1,
-        "sens_HRpolyp": 0.2,
-        "sens_stage_1": 0.79,
-        "sens_stage_2": 0.79,
-        "sens_stage_3": 0.79,
-        "sens_stage_4": 0.79,
-        "spec": 0.95,
-        "fpr": 0.05,
-        "c_test": 20,
-        "du_test": 0,
-        "p_complication": 0,
-        "du_complication": 0,
-        "p_death_complication": 0,
-        "c_complication": 0,
-    },
-    "sDNA": {
-        "sens_LRpolyp": 0.1,
-        "sens_HRpolyp": 0.2,
-        "sens_stage_1": 0.79,
-        "sens_stage_2": 0.79,
-        "sens_stage_3": 0.79,
-        "sens_stage_4": 0.79,
-        "spec": 0.95,
-        "fpr": 0.05,
-        "c_test": 20,
-        "du_test": 0,
-        "p_complication": 0,
-        "du_complication": 0,
-        "p_death_complication": 0,
-        "c_complication": 0,
-    },
-}
+import numpy as np
+
+import inputs as i
+import global_configs as c
+
+# ---------------------------------------------------------------------------- #
+# SCREENING SPECS
+# ---------------------------------------------------------------------------- #
+with open(i.CONFIGS_DIR / "screening_test_specs.json", "r") as f:
+    SCREENING_TEST_SPECS = json.load(f)
+
+# ---------------------------------------------------------------------------- #
+# SCREENING STRATEGIES
+# ---------------------------------------------------------------------------- #
+with open(i.CONFIGS_DIR / "screening_strategies.json", "r") as f:
+    SCREENING_STRATEGIES = json.load(f)
+
+NH_STRATS = SCREENING_STRATEGIES["NH_STRATS"]
+INTERDIGITATION_STRATS = SCREENING_STRATEGIES["INTERDIGITATION_STRATS"]
+NONINTERDIGITATION_STRATS = SCREENING_STRATEGIES["NONINTERDIGITATION_STRATS"]
 
 
-# STRAT TYPES:  NH, Colo only, Colo + FIT, Colo + sDNA
+# ---------------------------------------------------------------------------- #
+# STRATEGY CLASS
+# ---------------------------------------------------------------------------- #
 class Strategy:
-    def __init__(self, gene, strategy, strategy_type, stool_test=None):
+    def __init__(self, gene, strategy_type, colo=None, alt_test=None):
         self.gene = gene
-        self.strategy = strategy
-        self.strategy_type = strategy_type
-        self.start_age_colo = strategy[1]
-        self.interval_colo = strategy[0]
-        self.start_age_stool = strategy[3] if stool_test else None
-        self.interval_stool = strategy[2] if stool_test else None
-        self.stool_test = stool_test
+        self.strategy_type = strategy_type  # NH, Colo, or Colo+Alt
+        self.colo = colo  # dict like {"interval": 2, "start_age": 25}
+        self.alt_test = (
+            alt_test  # dict like {"interval": 1, "start_age": 26, "test": "FIT"}
+        )
         self.is_NH = strategy_type == "NH"
-        self.strategy_name = self._get_strategy_name()
+        self.check_valid_inputs()
 
-    def _get_strategy_name(self):
+    def __str__(self):
+        """
+        Return summary representation of strategy specs. This is returned with print(s).
+        """
         if self.is_NH:
-            strategy_name = "NH"
-        elif self.stool_test:
-            strategy_name = f"Colo Q{self.interval_colo}Y {self.stool_test} Q{self.interval_stool}, Start Age: {self.start_age_colo}"
-        elif self.stool_test is None:
-            strategy_name = (
-                f"Colo Q{self.interval_colo}Y, Start Age: {self.start_age_colo}"
+            return f"{self.gene}: NH"
+        out = [f"{self.gene}:"]
+        if self.colo:
+            out.append(f"Colo Q{self.colo['interval']}Y @ {self.colo['start_age']}")
+        if self.alt_test:
+            out.append(
+                f"+ {self.alt_test['test']} Q{self.alt_test['interval']}Y @ {self.alt_test['start_age']}"
             )
-        else:
-            logging.error("Strategy type not recognized")
-            strategy_name = "UNKNOWN"
-        return strategy_name
+        return " ".join(out)
 
     def __repr__(self):
-        return f"Strategy(gene={self.gene}, stool_test={self.stool_test}, strategy={self.strategy})"
+        """
+        Developer/debug representation — should look like valid Python code
+        to recreate the object. Returns this if you just type strategy in console or .py.
+        """
+        return (
+            f"Strategy(gene={self.gene!r}, strategy_type={self.strategy_type!r}, "
+            f"colo={self.colo!r}, alt_test={self.alt_test!r})"
+        )
 
+    def check_valid_inputs(self):
+        """
+        Check that the inputs for creating the strategy are valid according to our
+        preset rules from the global_configs.py file.
+        """
+        errors = []
 
-INTERDIGITATION_STRATS = {
-    "MLH1": [[2, 25, 1, 26], [4, 25, 2, 26], [2, 30, 1, 31], [4, 30, 2, 31]],
-    "MSH2": [[2, 25, 1, 26], [4, 25, 2, 26], [2, 30, 1, 31], [4, 30, 2, 31]],
-    "MSH6": [[2, 25, 1, 26], [4, 25, 2, 26], [2, 30, 1, 31], [4, 30, 2, 31]],
-    "PMS2": [[2, 25, 1, 26], [4, 25, 2, 26], [2, 35, 1, 36], [4, 30, 2, 31]],
-}
-NONINTERDIGITATION_STRATS = {
-    "MLH1": [[3, 35], [3, 25], [2, 25], [1, 25]],
-    "MSH2": [[3, 35], [3, 25], [2, 25], [1, 25]],
-    "MSH6": [[5, 40], [3, 40], [3, 35], [1, 25]],
-    "PMS2": [[5, 40], [4, 40], [3, 40], [3, 35], [1, 25]],
-}
-NH_STRATS = {
-    "MLH1": [[0, 25]],
-    "MSH2": [[0, 25]],
-    "MSH6": [[0, 25]],
-    "PMS2": [[0, 25]],
-}
-# Create dictionary of strategies
-strategies = {}
-# Add NH strategies
-for gene in NH_STRATS:
-    for strat in NH_STRATS[gene]:
-        temp = Strategy(gene=gene, strategy=strat, strategy_type="NH")
-        strategies[f"{gene} {temp.strategy_name}"] = temp
-# Add non-interdigitated strategies
-for gene in NONINTERDIGITATION_STRATS:
-    for strat in NONINTERDIGITATION_STRATS[gene]:
-        temp = Strategy(gene=gene, strategy=strat, strategy_type="Colo")
-        strategies[f"{gene} {temp.strategy_name}"] = temp
-# Add interdigitated strategies
-for test in ["FIT", "sDNA"]:
-    for gene in INTERDIGITATION_STRATS:
-        for strat in INTERDIGITATION_STRATS[gene]:
-            temp = Strategy(
-                gene=gene, strategy=strat, strategy_type=f"Colo{test}", stool_test=test
+        if self.gene not in c.GENES:
+            errors.append(
+                f"Invalid gene '{self.gene}' in strategy '{self}'. "
+                f"Allowed: {c.GENES}"
             )
-            strategies[f"{gene} {temp.strategy_name}"] = temp
+
+        if self.strategy_type not in ["NH", "Colo", "Colo+Alt"]:
+            errors.append(
+                f"Invalid strategy_type '{self.strategy_type}' for strategy '{self.strategy_type}'. "
+                f"Allowed: ['NH', 'Colo', 'Colo+Alt']"
+            )
+
+        if self.colo:
+            if self.colo["interval"] not in c.SCREENING_INTERVALS:
+                errors.append(
+                    f"Invalid colonoscopy interval '{self.colo['interval']}' in strategy '{self.strategy_type}'. "
+                    f"Allowed: {c.SCREENING_INTERVALS}"
+                )
+            if self.colo["start_age"] not in c.SCREENING_START_AGES:
+                errors.append(
+                    f"Invalid colonoscopy start_age '{self.colo['start_age']}' in strategy '{self.strategy_type}'. "
+                    f"Allowed: {c.SCREENING_START_AGES}"
+                )
+
+        if self.alt_test:
+            if self.alt_test["test"] not in c.ALT_TESTS:
+                errors.append(
+                    f"Invalid alternative test '{self.alt_test['test']}' in strategy '{self.strategy_type}'. "
+                    f"Allowed: {c.ALT_TESTS}"
+                )
+            if self.alt_test["interval"] not in c.SCREENING_INTERVALS:
+                errors.append(
+                    f"Invalid alternative test interval '{self.alt_test['interval']}' in strategy '{self.strategy_type}'. "
+                    f"Allowed: {c.SCREENING_INTERVALS}"
+                )
+            # TODO: define proper screening start ages for our alternative tests
+
+        if errors:
+            raise ValueError("\n".join(errors))
+
+    def create_events(self):
+        if self.is_NH:
+            return []
+        if self.colo and not self.alt_test:
+            events = np.arange(
+                self.colo["start_age"], c.END_AGE, step=self.colo["interval"]
+            )
+            return events
+
+
+# ---------------------------------------------------------------------------- #
+# CREATE STRATEGY OBJECTS FROM JSON
+# ---------------------------------------------------------------------------- #
+strategies = []
+
+# NH
+for gene, lst in NH_STRATS.items():
+    for _ in lst:
+        strategies.append(Strategy(gene, "NH"))
+
+# Colo only
+for gene, lst in NONINTERDIGITATION_STRATS.items():
+    for s in lst:
+        strategies.append(Strategy(gene, "Colo", colo=s["colo"]))
+
+# Interdigitation
+for gene, lst in INTERDIGITATION_STRATS.items():
+    for s in lst:
+        strategies.append(
+            Strategy(gene, "Colo+Alt", colo=s["colo"], alt_test=s["alt_test"])
+        )
+
+# Print them
+for s in strategies:
+    print(s)
